fdc0207825e0bce4bc1c39c9b46ea7b7907d89b2
Manika Joshi <manikajoshi500@gmail.com>
2023-12-26 13:27:23 +0530

    annotate on mobile

--- paint.js
+++ paint.js
@@ -139,72 +139,145 @@ function drawPolygon(position, sides, angle) {
 }
 
 function draw(position) {
-  var fillBox = document.getElementById("fillBox"),
-    shape = document.querySelector(
-      'input[type="radio"][name="shape"]:checked'
-    ).value,
-    polygonSides = document.getElementById("polygonSides").value,
-    polygonAngle = document.getElementById("polygonAngle").value,
-    lineCap = document.querySelector(
-      'input[type="radio"][name="lineCap"]:checked'
-    ).value,
-    eraseDim = document.getElementById("eraseArea").value,
-    composition = document.querySelector(
-      'input[type="radio"][name="composition"]:checked'
-    ).value;
+  var fillBox = document.getElementById("fillBox");
+  // shape = document.querySelector(
+  //   'input[type="radio"][name="shape"]:checked'
+  // ).value,
+  // polygonSides = document.getElementById("polygonSides").value,
+  // polygonAngle = document.getElementById("polygonAngle").value,
+  // lineCap = document.querySelector(
+  //   'input[type="radio"][name="lineCap"]:checked'
+  // ).value,
+  // eraseDim = document.getElementById("eraseArea").value,
+  // composition = document.querySelector(
+  //   'input[type="radio"][name="composition"]:checked'
+  // ).value;
 
-  context.lineCap = lineCap;
-  context.globalCompositeOperation = composition;
+  // context.lineCap = lineCap;
+  // context.globalCompositeOperation = composition;
 
-  if (shape === "eraser") {
-    erase(position, eraseDim);
+  // if (shape === "eraser") {
+  //   erase(position, eraseDim);
+  //}
+
+  // if (shape === "free") {
+  drawFree(position);
+  // }
+
+  // if (shape === "circle") {
+  //   drawCircle(position);
+  // }
+  // if (shape === "line") {
+  //   drawLine(position);
+  // }
+
+  // if (shape === "qcurve") {
+  //   drawQCurve(position);
+  // }
+
+  // if (shape === "polygon") {
+  //   drawPolygon(position, polygonSides, polygonAngle * (Math.PI / 180));
+  // }
+
+  // if (shape !== "line" && shape !== "free" && shape !== "bcurve") {
+  //   if (fillBox.checked) {
+  //     context.fill();
+  //     localStorage.savedPaintCanvas = JSON.stringify(canvas.toDataURL());
+  //   } else {
+  //     context.stroke();
+  //     localStorage.savedPaintCanvas = JSON.stringify(canvas.toDataURL());
+  //   }
+  // }
+}
+
+// function dragStart(event) {
+//   dragging = true;
+//   dragStartLocation = getCanvasCoordinates(event);
+//   backCtx.drawImage(canvas, 0, 0);
+//   takeSnapshot();
+//   context.beginPath();
+//   context.moveTo(dragStartLocation.x, dragStartLocation.y);
+//   canvas.addEventListener("mousemove", drag, false);
+// }
+
+// function drag(event) {
+//   var position;
+//   if (dragging === true) {
+//     restoreSnapshot();
+//     position = getCanvasCoordinates(event);
+//     draw(position);
+//   }
+
+//   if (dragging === true) {
+//     restoreSnapshot();
+//     var position;
+//     if (isTouchDevice) {
+//       position = getTouchCoordinates(event);
+//       // console.log("mobile");
+//     } else {
+//       position = getCanvasCoordinates(event);
+//       console.log("laptop");
+//     }
+//     draw(position);
+//   }
+//}
+
+// function dragStop(event) {
+//   dragging = false;
+//   restoreSnapshot();
+//   var position = getCanvasCoordinates(event);
+//   draw(position);
+// }
+
+function getTouchCoordinates(event) {
+  let x, y;
+
+  if (event.clientX && event.clientY) {
+    x = event.clientX;
+    y = event.clientY;
+  } else if (event.touches && event.touches.length > 0) {
+    x = event.touches[0].clientX;
+    y = event.touches[0].clientY;
+  } else {
+    console.error("Could not determine coordinates from the event.");
+    return { x: 0, y: 0 };
   }
 
-  if (shape === "free") {
-    drawFree(position);
-  }
-
-  if (shape === "circle") {
-    drawCircle(position);
-  }
-  if (shape === "line") {
-    drawLine(position);
-  }
-
-  if (shape === "qcurve") {
-    drawQCurve(position);
-  }
-
-  if (shape === "polygon") {
-    drawPolygon(position, polygonSides, polygonAngle * (Math.PI / 180));
-  }
-
-  if (shape !== "line" && shape !== "free" && shape !== "bcurve") {
-    if (fillBox.checked) {
-      context.fill();
-      localStorage.savedPaintCanvas = JSON.stringify(canvas.toDataURL());
-    } else {
-      context.stroke();
-      localStorage.savedPaintCanvas = JSON.stringify(canvas.toDataURL());
-    }
-  }
+  return {
+    x: x - canvas.getBoundingClientRect().left,
+    y: y - canvas.getBoundingClientRect().top,
+  };
 }
 
 function dragStart(event) {
   dragging = true;
-  dragStartLocation = getCanvasCoordinates(event);
+  if (isTouchDevice) {
+    dragStartLocation = getTouchCoordinates(event);
+    console.log("mobile");
+  } else {
+    dragStartLocation = getCanvasCoordinates(event);
+    console.log("laptop");
+  }
   backCtx.drawImage(canvas, 0, 0);
   takeSnapshot();
   context.beginPath();
   context.moveTo(dragStartLocation.x, dragStartLocation.y);
-  canvas.addEventListener("mousemove", drag, false);
 }
 
 function drag(event) {
-  var position;
   if (dragging === true) {
     restoreSnapshot();
-    position = getCanvasCoordinates(event);
+    // if (isTouchDevice) {
+    //   // draglaptop(event);
+    //   position = getTouchCoordinates(event);
+    //   // console.log("mobile");
+    // }
+
+    var position;
+    //else {
+    position = getTouchCoordinates(event);
+    // console.log("laptop");
+    // }
     draw(position);
   }
 }
@@ -212,58 +285,86 @@ function drag(event) {
 function dragStop(event) {
   dragging = false;
   restoreSnapshot();
-  var position = getCanvasCoordinates(event);
+  var position;
+  if (isTouchDevice) {
+    position = getTouchCoordinates(event);
+    console.log("mobile");
+  } else {
+    position = getCanvasCoordinates(event);
+    console.log("laptop");
+  }
   draw(position);
 }
 
-function changeLineWidth() {
-  context.lineWidth = this.value;
-  event.stopPropagation();
-}
+// function changeLineWidth() {
+//   context.lineWidth = this.value;
+//   event.stopPropagation();
+// }
 
-function changeFillStyle() {
-  context.fillStyle = this.value;
-  event.stopPropagation();
-}
+// function changeFillStyle() {
+//   context.fillStyle = this.value;
+//   event.stopPropagation();
+// }
 
-function changeStrokeStyle() {
-  context.strokeStyle = this.value;
-  event.stopPropagation();
-}
+// function changeStrokeStyle() {
+//   context.strokeStyle = this.value;
+//   event.stopPropagation();
+// }
 
-function changeBackgroundColor() {
-  context.save();
-  context.fillStyle = document.getElementById("backgroundColor").value;
-  context.fillRect(0, 0, canvas.width, canvas.height);
-  localStorage.savedPaintCanvas = JSON.stringify(canvas.toDataURL());
-  context.restore();
-}
+// function changeBackgroundColor() {
+//   context.save();
+//   context.fillStyle = document.getElementById("backgroundColor").value;
+//   context.fillRect(0, 0, canvas.width, canvas.height);
+//   localStorage.savedPaintCanvas = JSON.stringify(canvas.toDataURL());
+//   context.restore();
+// }
 
 function eraseCanvas() {
   context.clearRect(0, 0, canvas.width, canvas.height);
   backCtx.clearRect(0, 0, backCanvas.width, backCanvas.height);
 }
 
+var isTouchDevice = "ontouchstart" in document.documentElement;
 function init() {
   canvas = document.getElementById("canvas");
   context = canvas.getContext("2d");
-  var lineWidth = document.getElementById("lineWidth"),
-    fillColor = document.getElementById("fillColor"),
-    strokeColor = document.getElementById("strokeColor"),
-    canvasColor = document.getElementById("backgroundColor"),
-    clearCanvas = document.getElementById("clearCanvas");
-  deleteLastStep = document.getElementById("deleteLastStep");
-  context.strokeStyle = strokeColor.value;
-  context.fillStyle = fillColor.value;
-  context.lineWidth = lineWidth.value;
+
+  // Set default values
+  context.strokeStyle = "#000000"; // Black color
+  context.lineWidth = 2; // Default line width
+
+  // Other UI elements initialization
+  var clearCanvas = document.getElementById("clearCanvas"),
+    deleteLastStep = document.getElementById("deleteLastStep");
+
+  //  fillColor = document.getElementById("fillColor"),
+  //    canvasColor = document.getElementById("backgroundColor"),
+  //    clearCanvas = document.getElementById("clearCanvas");
+  //  deleteLastStep = document.getElementById("deleteLastStep");
 
   canvas.addEventListener("mousedown", dragStart, false);
   canvas.addEventListener("mousemove", drag, false);
   canvas.addEventListener("mouseup", dragStop, false);
-  lineWidth.addEventListener("input", changeLineWidth, false);
-  fillColor.addEventListener("input", changeFillStyle, false);
-  strokeColor.addEventListener("input", changeStrokeStyle, false);
-  canvasColor.addEventListener("input", changeBackgroundColor, false);
+
+  canvas.addEventListener("touchstart", dragStart, false);
+  canvas.addEventListener("touchmove", drag, false);
+  canvas.addEventListener("touchend", dragStop, false);
+
+  // if (isTouchDevice) {
+  //   canvas.addEventListener("touchstart", dragStart, { passive: true });
+  //   canvas.addEventListener("touchmove", drag, { passive: true });
+  //   // For touchend, you don't need to specify the passive option because touchend is not cancellable
+  //   canvas.addEventListener("touchend", dragStop);
+  // } else {
+  //   canvas.addEventListener("mousedown", dragStart, false);
+  //   canvas.addEventListener("mousemove", drag, false);
+  //   canvas.addEventListener("mouseup", dragStop, false);
+  // }
+
+  // lineWidth.addEventListener("input", changeLineWidth, false);
+  // fillColor.addEventListener("input", changeFillStyle, false);
+  // strokeColor.addEventListener("input", changeStrokeStyle, false);
+  // canvasColor.addEventListener("input", changeBackgroundColor, false);
   clearCanvas.addEventListener("click", eraseCanvas, false);
   deleteLastStep.addEventListener("click", deleteStep, false);
   // canvas.addEventListener("click", handleAnnotation); //can be deleted mp
@@ -443,8 +544,9 @@ function downloadAnnotations() {
 var downloadButton = document.getElementById("downloadButton");
 downloadButton.addEventListener("click", downloadAnnotations);
 
-var masks = [];
-function generateMask() {
+function saveMasksAsImages() {
+  var masks = [];
+
   // Iterate over each set of freehand points (each annotation)
   for (
     var annotationIndex = 0;
@@ -495,40 +597,12 @@ function generateMask() {
   }
 
   console.log(masks);
-}
-
-var maskButton = document.getElementById("maskButton");
-maskButton.addEventListener("click", generateMask);
-
-function saveMasksAsImages() {
-  // Create a new canvas to combine masks
-  // const combinedCanvas = document.createElement('canvas');
-  // const combinedContext = combinedCanvas.getContext('2d');
-
-  // // Set canvas dimensions (assuming both masks have the same dimensions)
-  // combinedCanvas.width = masks[0].width;
-  // combinedCanvas.height = masks[0].height;
-
-  // // Draw each mask onto the combined canvas
-  // for (let i = 0; i < masks.length; i++) {
-  //     combinedContext.putImageData(masks[i], 0, 0);
-  // }
-
-  // // Convert the combined canvas to a data URL
-  // const combinedImageDataUrl = combinedCanvas.toDataURL('image/png');
-
-  // // Create an anchor element for download
-  // const downloadAnchor = document.createElement('a');
-  // downloadAnchor.href = combinedImageDataUrl;
-  // downloadAnchor.download = 'combined_masks.png';  // Name of the downloaded file
-  // downloadAnchor.click();
 
   const combinedCanvas = document.createElement("canvas");
   combinedCanvas.width = canvas.width;
   combinedCanvas.height = canvas.height;
   const combinedContext = combinedCanvas.getContext("2d");
 
-  // Iterate over masks and composite each onto the combined canvas
   masks.forEach(function (maskImageData) {
     // Create a temporary canvas for the current mask
     const tempCanvas = document.createElement("canvas");
@@ -549,9 +623,58 @@ function saveMasksAsImages() {
   downloadAnchor.click();
 }
 
+var maskDownload = document.getElementById("maskDownloadButton");
+maskDownload.addEventListener("click", saveMasksAsImages);
+
+// function saveMasksAsImages() {
+//   // Create a new canvas to combine masks
+//   // const combinedCanvas = document.createElement('canvas');
+//   // const combinedContext = combinedCanvas.getContext('2d');
+
+//   // // Set canvas dimensions (assuming both masks have the same dimensions)
+//   // combinedCanvas.width = masks[0].width;
+//   // combinedCanvas.height = masks[0].height;
+
+//   // // Draw each mask onto the combined canvas
+//   // for (let i = 0; i < masks.length; i++) {
+//   //     combinedContext.putImageData(masks[i], 0, 0);
+//   // }
+
+//   // // Convert the combined canvas to a data URL
+//   // const combinedImageDataUrl = combinedCanvas.toDataURL('image/png');
+
+//   // // Create an anchor element for download
+//   // const downloadAnchor = document.createElement('a');
+//   // downloadAnchor.href = combinedImageDataUrl;
+//   // downloadAnchor.download = 'combined_masks.png';  // Name of the downloaded file
+//   // downloadAnchor.click();
+
+//   const combinedCanvas = document.createElement("canvas");
+//   combinedCanvas.width = canvas.width;
+//   combinedCanvas.height = canvas.height;
+//   const combinedContext = combinedCanvas.getContext("2d");
+
+//   // Iterate over masks and composite each onto the combined canvas
+//   masks.forEach(function (maskImageData) {
+//     // Create a temporary canvas for the current mask
+//     const tempCanvas = document.createElement("canvas");
+//     tempCanvas.width = combinedCanvas.width;
+//     tempCanvas.height = combinedCanvas.height;
+//     const tempContext = tempCanvas.getContext("2d");
+
+//     // Draw the current mask onto the temporary canvas
+//     tempContext.putImageData(maskImageData, 0, 0);
+
+//     // Composite the temporary canvas onto the combined canvas
+//     combinedContext.drawImage(tempCanvas, 0, 0);
+//   });
+
+//   const downloadAnchor = document.getElementById("downloadAnchor");
+//   downloadAnchor.href = combinedCanvas.toDataURL("image/png");
+//   downloadAnchor.download = "combined_masks.png";
+//   downloadAnchor.click();
+// }
+
 // function funct(){
 //     saveMasksAsImages(masks, 'mask');
 // }
-
-var maskDownload = document.getElementById("maskDownloadButton");
-maskDownload.addEventListener("click", saveMasksAsImages);
